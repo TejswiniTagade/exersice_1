04d9332a58ffa9b41c5c61435d90f919459ab710
TejswiniTagade <76645740+TejswiniTagade@users.noreply.github.com>
2020-12-27 15:15:50 +0530

    added 3 lines of code.

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind("192.168.43.52", 1234)
+s.bind(("192.168.43.52", 1234))
+
+s.listen(10)
+c, addr = s.accept()
+print("Connected")
+
